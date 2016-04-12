6219751a10a926f8ed328b65cd3a82c1032a876c
Barakar <babarakariel@gmail.com>
2016-04-13 00:07:43 +0300

    making friendList to return the state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';  
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 
 import configureStore from '../store/configureStore';
 import DevTools from './DevTools';
--- src/containers/FriendListApp.js
+++ src/containers/FriendListApp.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { combineReducers } from 'redux';  
-import { Provider } from 'react-redux';
+import { Provider,connect } from 'react-redux';
 
-
+@connect(state => { 
+	return {
+  friendlist: state.friends.friendsById
+}})
 export default class App extends Component {
 render() {
     return (
-      <h1>Hello, world.</h1>
+      <h1>{JSON.stringify(this.props.friendlist, null, 2)}</h1>
     );
   }
 }
